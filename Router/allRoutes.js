b0d5dc8374525c6bbb46b738297730b4acfe65c2
loki-patera <boulet.g@outlook.be>
2024-05-26 12:28:20 +0200

    Add galerie page

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -2,7 +2,8 @@ import Route from "./Route.js"
 
 // Definition of routes here
 export const allRoutes = [
-  new Route("/", "Accueil", "/pages/home.html")
+  new Route("/", "Accueil", "/pages/home.html"),
+  new Route("/galerie", "La galerie", "/pages/galerie.html")
 ]
 
 // The title is displayed like this : Route.title - websitename
